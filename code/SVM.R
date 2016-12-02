ec8c0e3256250fda629c1f47d938f23cf2e37a39
Pranay Singal <psingal@berkeley.edu>
2016-12-02 08:16:20 -0800

    Add files via upload

--- code/SVM.R
+++ code/SVM.R
@@ -2,47 +2,54 @@
 # SVM
 ############################################################
 
+# Load necessary libraries
+library(e1071)
+library(parallel)
+
 # Setwd to root directory of project before reading data
-setwd("~/Fall 2016/STAT 154/Final Project/STAT154-Group04")
+setwd("~/Fall 2016/STAT 154/Final Project")
 
 # Read training data
-train_df1 <- read.csv("data/processed_train_df.csv")
-train_df2 <- read.csv("data/processed_train_df_2.csv")
-train_df3 <- read.csv("data/processed_train_df_3.csv")
-
-train_df1 <- train_df1[,-1]
-train_df2 <- train_df2[,-1]
-train_df3 <- train_df3[,-1]
-
-train_df1[,1] <- as.factor(train_df1[,1])
-train_df2[,1] <- as.factor(train_df2[,1])
-train_df3[,1] <- as.factor(train_df3[,1])
+#train_df <- read.csv("STAT154-Group04/data/processed_train_df_4.csv")
+#train_df <- train_df[,-1]
+#train_df[,1] <- as.factor(train_df[,1])
 
 # First column is target variable
 # Second is number of words 
 # Remaining are word frequencies
 
-# Load necessary libraries
-library(e1071)
-library(parallel)
-
 # Give variables names that are specific to model (SVM_mse instead of mse)
 
-### Functions ###
-svm_sum_rad <- function(data_set, cost_vec = 0, gamma_vec = 0, num_folds){
-  tune.out <- tune(svm, emails.V1~ ., data = data_set, kernel = "radial",
-                   ranges = list(cost = cost_vec, gamma = gamma_vec),
-                   tunecontrol = tune.control(cross = num_folds))
+############################
+######## FUNCTION ##########
+############################
+
+sum_lin <- function(i){
+  tune.out <- tune(svm, emails.V1~ ., data = train_df, kernel = "linear", cost = cost_vec[i],
+                   tunecontrol = tune.control(cross = 5))
   sum_tune.out <- summary(tune.out)
-  return(sum_tune.out)
+  svm_error <- sum_tune.out$best.performance
+  names(svm_error) <- paste0("cost=", cost_vec[i])
+  return(svm_error)
 }
 
-svm_sum <- function(kernel_method, data_set, cost_vec = 0, num_folds, num_degree = 0){
-  tune.out <- tune(svm, emails.V1~ ., data = data_set, kernel = kernel_method, degree = num_degree,
-                   ranges = list(cost = cost_vec),
-                   tunecontrol = tune.control(cross = num_folds))
+sum_poly <- function(i){
+  tune.out <- tune(svm, emails.V1~ ., data = train_df, kernel = "polynomial", cost = cost_vec[i], degree = 2,
+                   tunecontrol = tune.control(cross = 5))
   sum_tune.out <- summary(tune.out)
-  return(sum_tune.out)
+  svm_error <- sum_tune.out$best.performance
+  names(svm_error) <- paste0("cost=", cost_vec[i])
+  return(svm_error)
+}
+
+sum_rad <- function(i){
+  tune.out <- tune(svm, emails.V1~ ., data = train_df, kernel = "radial",
+                   cost = cost_gamma[[i]][1], gamma = cost_gamma[[i]][2],
+                   tunecontrol = tune.control(cross = 5))
+  sum_tune.out <- summary(tune.out)
+  svm_error <- sum_tune.out$best.performance
+  names(svm_error) <- paste("cost=", cost_gamma[[i]][1],"and","gamma=", cost_gamma[[i]][2], sep = " ")
+  return(svm_error)
 }
 
 rem_columns <- function(data_set, col_start, cond_num){
@@ -59,87 +66,73 @@ rem_columns <- function(data_set, col_start, cond_num){
   return(data_set)
 }
 
-### Global Variables ###
-kernels <- c("radial", "linear", "polynomial")
-cost_vec <- c(.05, .1, .15, 1, 10)
-cost_vec_poly <- c(.05, .1, .15, 1, 10, 100)
-gamma_vec <- c(.1, .5, 1)
+############################
+##### GLOBAL VARIABLES #####
+############################
 
-#### DATA SET 1 ####
-sum_svm_rad_1 <- svm_sum_rad(train_df1, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_svm_lin_1 <- svm_sum(kernels[2], train_df1, cost_vec = cost_vec_poly, num_folds = 5)
-sum_svm_pol_1 <- svm_sum(kernels[3], train_df1, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+cost_vec <- c(.1, .5, 1, 10, 100)
+gamma_vec <- c(.1, .5, 1, 5)
+cost_gamma <- list()
 
-#### DATA SET 2 ####
-sum_svm_rad_2 <- svm_sum_rad(train_df2, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_svm_lin_2 <- svm_sum(kernels[2], train_df2, cost_vec = cost_vec_poly, num_folds = 5)
-sum_svm_pol_2 <- svm_sum(kernels[3], train_df2, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+for(i in 1:length(cost_vec)){
+  for(j in 1:length(gamma_vec)){
+    cost_gamma[[length(cost_gamma)+1]] <- c(cost_vec[i], gamma_vec[j])
+  }
+}
 
-#### DATA SET 3 ####
-sum_svm_rad_3 <- svm_sum_rad(train_df3, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_svm_lin_3 <- svm_sum(kernels[2], train_df3, cost_vec = cost_vec_poly, num_folds = 5)
-sum_svm_pol_3 <- svm_sum(kernels[3], train_df3, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+#######################
+######## svm ##########
+#######################
 
+train_df <- read.csv("STAT154-Group04/data/processed_train_df_2.csv")
+train_df <- train_df[,-1]
+train_df[,1] <- as.factor(train_df[,1])
 
-###### MODIFIED DATA SET 1 ######
-# We are only looking at the columns that have more than 5 occurrences
-mod5_train_df1 <- rem_columns(train_df1, 3, 5)
-mod5_train_df2 <- rem_columns(train_df2, 5, 5)
-mod5_train_df3 <- rem_columns(train_df3, 5, 5)
+train_df <- rem_columns(train_df, 4, 9)
 
-sum_rad1_mod5 <- svm_sum_rad(mod5_train_df1, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_lin1_mod5 <- svm_sum(kernels[2], mod5_train_df1, cost_vec = cost_vec_poly, num_folds = 5)
-sum_pol1_mod5 <- svm_sum(kernels[3], mod5_train_df1, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+cl <- makeCluster(3)
+clusterExport(cl, "train_df")
+clusterExport(cl, "cost_vec")
+clusterExport(cl, "cost_gamma")
+clusterEvalQ(cl, library(e1071))
+svm_lin_df3 <- parSapply(cl, 1:length(cost_vec), FUN = sum_lin)
+svm_poly_df3 <- parSapply(cl, 1:length(cost_vec), FUN = sum_poly)
+svm_rad_df3 <- parSapply(cl, 1:length(cost_gamma), FUN = sum_rad)
+stopCluster(cl)
 
-sum_rad2_mod5 <- svm_sum_rad(mod5_train_df2, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_lin2_mod5 <- svm_sum(kernels[2], mod5_train_df2, cost_vec = cost_vec_poly, num_folds = 5)
-sum_pol2_mod5 <- svm_sum(kernels[3], mod5_train_df2, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+############################
+######## ACCURACY ##########
+############################
 
-sum_rad3_mod5 <- svm_sum_rad(mod5_train_df3, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_lin3_mod5 <- svm_sum(kernels[2], mod5_train_df3, cost_vec = cost_vec_poly, num_folds = 5)
-sum_pol3_mod5 <- svm_sum(kernels[3], mod5_train_df3, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+svm_lin_bestmod <- svm(emails.V1~ ., data = train_df, kernel = "linear", cost = .1,
+                       tunecontrol = tune.control(cross = 5))
 
+train_pred <- predict(svm_lin_bestmod, newdata = train_df)
+acc_matrix <- table(train_df[,1], train_pred)
 
-###### MODIFIED DATA SET 2 ######
-# We are only looking at the columns that have more than 10 occurrences
-mod10_train_df1 <- rem_columns(train_df1, 3, 10)
-mod10_train_df2 <- rem_columns(train_df2, 5, 10)
-mod10_train_df3 <- rem_columns(train_df3, 5, 10)
-
-sum_rad1_mod10 <- svm_sum_rad(mod10_train_df1, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_lin1_mod10 <- svm_sum(kernels[2], mod10_train_df1, cost_vec = cost_vec_poly, num_folds = 5)
-sum_pol1_mod10 <- svm_sum(kernels[3], mod10_train_df1, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
-
-sum_rad2_mod10 <- svm_sum_rad(mod10_train_df2, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_lin2_mod10 <- svm_sum(kernels[2], mod10_train_df2, cost_vec = cost_vec_poly, num_folds = 5)
-sum_pol2_mod10 <- svm_sum(kernels[3], mod10_train_df2, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
-
-sum_rad3_mod10 <- svm_sum_rad(mod10_train_df3, cost_vec = cost_vec, gamma_vec = gamma_vec, 5)
-sum_lin3_mod10 <- svm_sum(kernels[2], mod10_train_df3, cost_vec = cost_vec_poly, num_folds = 5)
-sum_pol3_mod10 <- svm_sum(kernels[3], mod10_train_df3, cost_vec = cost_vec_poly, num_folds = 5, num_degree = 2)
+total_acc <- sum(diag(acc_matrix)) / nrow(train_df)
 
 
+############################
+######## PREDICTON #########
+############################
 
+# Load and modify test data set
+test_df <- read.csv("STAT154-Group04/data/processed_test_df_2.csv")
+test_df <- test_df[,-1]
 
+col_names_train <- colnames(train_df)
+col_names_train <- col_names_train[-1] #remove emails.v1
 
+colnames(test_df)[1:3] <- col_names_train[1:3]
+test_df <- test_df[,which(colnames(test_df) %in% col_names_train)]
 
-
+pred.te <- predict(svm_lin_bestmod, newdata = test_df)
+test_pred <- data.frame(pred.te)
+write.table(x = pred.te, file = "predict.txt", row.names = FALSE, col.names = FALSE)
 
 ############################################################
 # Save image in Rdata so it can be loaded in other scripts
-save.image(file="data/SVM.Rdata")
+save.image(file="STAT154-Group04/data/SVM.Rdata")
+load("STAT154-Group04/data/SVM.Rdata")
 ############################################################
-
-
-
-
-
-
-#Kensen code for Parallel
-cl <- makeCluster(4) #use four cores for this job, if you have more cores, use a higher number
-clusterExport(cl, "train_df")
-clusterExport(cl, "impt")
-clusterExport(cl, "ntree")
-clusterEvalQ(cl, library(randomForest))
-OOBerror <- parSapply(cl, 1:4 , rf) 
-stopCluster(cl)
